0fa7a17306cc31fc097aefb4695b9a44f29cc0b8
waleed-industrielleads <waleed@industrielleads.com>
2024-03-12 15:42:55 +0400

    Changed api urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const statusArray = ["applied", "declined", "interview", "rejected", "converted"];
 
-export const JOBS_URL = "http://localhost:3000/api/v1/jobs"
-export const AUTH_URL = "http://localhost:3000/api/v1/auth"
+export const JOBS_URL = "https://sore-red-bullfrog-gear.cyclic.app/api/v1/jobs"
+export const AUTH_URL = "https://sore-red-bullfrog-gear.cyclic.app/api/v1/auth"
